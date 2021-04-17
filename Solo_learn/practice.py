035d4f73df48723aedfc70b38b2f942686316c49
Abhisehk <abhishekcusat11@gmail.com>
2021-04-18 00:27:47 +0530

    commit

--- Solo_learn/practice.py
+++ Solo_learn/practice.py
@@ -6,7 +6,29 @@
 #   print("Looping...")
 
 
-def print_double(x):
-       print(2 * x)
+# def print_double(x):
+#        print(2 * x)
 
-print_double(3)
+# print_double(3)
+
+# test = { }
+# print(test[0])
+
+
+# fib = {1: 1, 2: 1, 3: 2, 4: 3}
+# print(fib.get(4, 0) + fib.get(7, 5))
+# print("{0}{1}{0}".format("abra", "cad"))
+# str="{c}, {b}, {a}".format(a=5, b=9, c=7)
+# print(str)
+
+# a=min([sum([11, 22]), max(abs(-30), 2)])
+# print(a)
+
+# nums = [-1, 2, -3, 4, -5]
+# if all([abs(i) < 3 for i in nums]):
+#   print(1)
+# else:
+#   print(2)
+
+nums = (55, 44, 33, 22)
+print(max(min(nums[:2]), abs(-42)))
