f7e2cfcdb83231b68819331999ad176b7b6f3adb
Abhisehk <abhishekcusat11@gmail.com>
2021-04-20 16:22:40 +0530

    commit

--- Solo_learn/practice.py
+++ Solo_learn/practice.py
@@ -150,3 +150,202 @@
 #     print("Zero")
 # except:
 #     print("some")
+# print('Milk' >"Milkey")
+# 
+# class A:
+#     def __init__(self,v=1):
+#         self.v=v
+#     def set(self ,v):
+#         self.v=v
+#         return v
+# a=A()
+# print(a.set(a.v+1))
+# class A:
+#     def __str__(self):
+#         return 'a'
+# class B:
+#     def __str__(self):
+#         return 'b'
+# class C(A,B):
+#     pass
+# o = C()
+# print(o)
+
+
+# class A:
+#     pass
+# class B(A):
+#     pass
+# class C(B):
+#     pass
+# print(issubclass(C,A))
+# class A:
+#         def __init__(self):
+#             self.a=1
+# class B(A):
+#         def __init__(self):
+#             self.b=2
+# l=[0,1,2,3,4,5,6]
+# foo=tuple(filter(lambda x:x-0 and x-1 ,l))
+# print(foo)
+# b=bytearray(3)
+# print(b)
+# import calendar
+# print(calendar.weekheader(2))
+# from datetime import date
+# date_1=date(1992,1,16)
+# date2=date(1991,2,5)
+# print(date_1-date2)
+# from datetime import datetime
+# datetime=datetime(2019 ,11,27,11,27,22)
+# print(datetime.strftime('%Y/%B/%d%H:%M:%S'))
+# def fun(n):
+#     s='+'
+#     for i in range(n):
+#         s+=s
+#         yield s
+
+# for x in fun(2):
+#     print(x,end='')
+# import os
+# os.mkdir('thumbnails')
+# os.chdir('thumbnails')
+# sizes=['small' ,'medium' ,'large']
+# for size in sizes:
+#     os.mkdir(size)
+# print(os.listdir())
+# def o(p):
+#     def q():
+#         return '*' * p
+#     return q
+# r=o(1)
+# s=o(2)
+# print(r() + s())
+# import os
+# os.mkdir('pictures')
+# os.chdir('pictures')
+# os.mkdir('thumbnails')
+# os.chdir('thumbnails')
+# os.mkdir('tmp')
+# os.chdir('../')
+# print(os.getcwd())
+
+# import calendar
+# c=calendar.Calendar()
+# for weekday in c.iterweekdays():
+#     print(weekday ,end= " ")
+
+# def I():
+#     s='abcdef'
+#     for c in s[::2]:
+#         yield c
+# for x in I():
+#     print(x,end='')
+# class A:
+#     def __init__(self, v=2):
+#         self.v=v
+#     def set(self ,v=1):
+#         self.v +=v
+#         return self.v
+# a=A()
+# b=a 
+# b.set()
+# print(a.v)
+# import calendar
+# calendar.setfirstweekday(calendar.SUNDAY)
+# print(calendar.weekheader(3))
+# print(float("1.3"))
+# import os
+# os.mkdir('new')
+# os.chdir('new')
+# print(os.getcwd)
+# from datetime import datetime
+# date1=datetime(2019 , 11, 27,11,27,22)
+# date2=datetime(2019,11,27,0,0,0)
+# print(date1-date2)4
+# x="\\\\"
+# print(len(x))
+# try:
+#     raise Exception
+# except:
+#     print("c")
+
+# except BaseException:
+#     print("a")
+
+# except Exception:
+#     print("b")
+# x="\\\"
+# print(len(x))
+
+# import random
+# a=random.randrange(10,100,3)
+# b=random.randint(0,100)  
+# c=random.choice((0,100,3)) 
+# print(__name__)
+# # print(a , b, c)
+# try:
+#     raise Exception(1,2,3)
+# except Exception as e:
+#     print(len(e.args))
+# n=[0,2,7,9,10]
+# foo=map(lambda num: num**2 ,n)
+# print(list(foo))
+
+# class A:
+#     A=1
+#     def __init__(self):
+#         self.a=0;
+# print(hasattr(A, 'a'))
+
+# from datetime import timedelta
+# delta=timedelta(weeks=1 ,days=7 ,hours=11)
+# print(delta*2)
+# n=[i*i for i in range(5)]
+# foo=list(filter(lambda num: num%2 ,n))
+# print(foo)
+# class A:
+#     pass
+# class B(A):
+#     pass
+# class C(B):
+#     pass
+# print(issubclass(A,C))
+# import os 
+# os.mkdir('pictures')
+# os.chdir('pictures')
+# print(os.getcwd)
+# s1='Bond'
+# s2='James Bond'
+# print(s1.isalpha() ,s2.isalpha())
+# class A:
+
+#     def __init__(self):
+
+#         pass
+    
+
+#     def f(self):
+#         return 1
+    
+#     def g():
+#         return self.f()
+
+# a=A()
+# # print(a.g())
+
+# import os
+# os.makedirs('pic/thu')
+# # os.rmdir('pic')
+
+# # import calendar
+# # c=calendar.Calendar(calendar.SUNDAY)
+# # for weekday in c.iterweekdays():
+# from datetime import timedelta
+# delta =timedelta(weeks=1 ,days= 7, hours=11)
+# print(delta)
+# #     print(weekday,end=" ")
+
+y=input()
+x=input()
+print(x+y)
